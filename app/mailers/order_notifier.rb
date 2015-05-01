6bb19f67ce960b3bd6b32fc7c000cdf4328923a3
Kacper Trafial <kacper.trafial@live.com>
2015-05-01 22:52:36 +0200

    Naprawda błędów Mailera
    Efekt: brak błędów przy testach

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: 'Sam Ruby <depot@example.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,7 +9,7 @@ class OrderNotifier < ActionMailer::Base
   def reveived(order)
     @order = order
 
-    mail(to: @order.mail, subject: 'Pragmatic Store Order Confirmation')
+    mail(to: order.email, subject: 'Pragmatic Store Order Confirmation')
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -20,6 +20,6 @@ class OrderNotifier < ActionMailer::Base
   def shipped(order)
     @order = order
 
-    mail(to: @order.mail, subject: 'Pragmatic Store Order Confirmation')
+    mail(to: order.email, subject: 'Pragmatic Store Order Shipped' )
   end
 end
