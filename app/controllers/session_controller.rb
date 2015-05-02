39ba70353eebd0f131a11eaf472cb6d836dbb995
Kacper Trafial <kacper.trafial@live.com>
2015-05-02 16:20:58 +0200

    Usprawnienie działania kontrolera sesji

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,8 +3,17 @@ class SessionController < ApplicationController
   end
 
   def create
+    uset = Uset.find_by(name: params[:name])
+    if uset and uset.authenticate(params[:password])
+      session[:uset_id] = uset.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid user/password combination"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to store_url, notice: "Logged out"
   end
 end
